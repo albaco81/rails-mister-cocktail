b8d597f0a143b6808b0700b518447de568acc71c
Alvaro Diaz <albaco81@hotmail.com>
2017-08-17 17:19:57 +0200

    progress day 1

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
